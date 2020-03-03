01d8d768a404e42d43741c8de2392011ef94de82
Krushna <krushnasinh.jadeja14545@marwadieducation.edu.in>
2020-03-03 17:08:42 +0530

    Updates

---
+++ precache-manifest.764eded6d2f4c7c19a26907bfa609a16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7054d50606edfd53627a5a4a70c25cb5",
+    "url": "/support-ticket/index.html"
+  },
+  {
+    "revision": "44150e5c98dcac6971f9",
+    "url": "/support-ticket/static/css/2.28a3199c.chunk.css"
+  },
+  {
+    "revision": "5555c70970d2e307fc78",
+    "url": "/support-ticket/static/css/main.cb18d035.chunk.css"
+  },
+  {
+    "revision": "44150e5c98dcac6971f9",
+    "url": "/support-ticket/static/js/2.cb05f23b.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/support-ticket/static/js/2.cb05f23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5555c70970d2e307fc78",
+    "url": "/support-ticket/static/js/main.3d3c0bdc.chunk.js"
+  },
+  {
+    "revision": "fd6d06b87c872de2ab9f",
+    "url": "/support-ticket/static/js/runtime-main.bb9d8723.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/support-ticket/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/support-ticket/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/support-ticket/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/support-ticket/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/support-ticket/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
